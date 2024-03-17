0ddb6dc240123eb769c95d5d2b35bd907c162a24
Jatin Foujdar <jatin321001@gmail.com>
2024-03-17 23:42:41 +0530

    first commit

---
+++ Example/file.js
@@ -0,0 +1,31 @@
+const { log } = require("console");
+const fs = require("fs");
+
+
+//Async
+// fs.writeFile("./test.txt", "Hello World Async",(err))
+
+// const result = fs.readFileSync("./contact.txt", "utf-8")
+// console.log(result);
+
+// fs.appendFileSync("/text.txt", `${Data.now()}hey there/n`)
+
+// fs.cpSync("./text.txt", "./copy.txt");
+// fs.unlinkSync("./copy.txt")
+//////////////
+
+
+// console.log(1);
+// blocking
+// const result = fs.readFileSync("contact.txt", "utf-8",);
+//     console.log();
+// })
+//  console.log(2);
+
+
+// console.log(1);
+// // non-blocking
+// fs.readFile("contact.txt", "utf-8",(err,result)=>{
+//     console.log(result);
+// })
+// console.log(2);
