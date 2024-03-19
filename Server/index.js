30fd07b3e78f195bdcd796726cdcca2e1f675c4a
Jatin Foujdar <jatin321001@gmail.com>
2024-03-19 23:31:17 +0530

    commit

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,17 @@
 const http = require("http");
 const fs = require("fs");
 
-const MyServer = http.createServer((req,res)=>{
-    const log= `${Date.now()}: new req`;
-fs.appendFile("log.txt",log,(err,data)=>{
-    
-    res.end("Hello form server")
-})
-   
+const MyServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} new req\n`; 
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) {
+            console.error("Error writing to log file:", err);
+            res.statusCode = 500;
+            res.end("Internal Server Error");
+        } else {
+            res.end("Hello from server");
+        }
+    });
 });
 
-MyServer.listen(8000,()=>console.log("Server Started!"));
-
-
+MyServer.listen(8000, () => console.log("Server Started!"));
